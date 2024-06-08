09bf38f061576283c0d1383e53a03b492784991c
Diego Alejandro Torres Fuerte <dtorresf@redhat.com>
2024-06-07 21:47:22 -0600

    pipeline file completed

--- source_code/40_pipelines/train-car-rekon.py
+++ source_code/40_pipelines/train-car-rekon.py
@@ -16,247 +16,100 @@ base_image = os.getenv(
     base_image=base_image,
     packages_to_install=["requests", "zipp"],
 )
-def download_data(dataset_type: str, 
-                  datasets_zip: dsl.Output[dsl.Dataset],
-                  datasets_dir: dsl.Output[dsl.Dataset]):
+def download_data(dataset_type: str,
+                  datasets: dsl.Output[dsl.Dataset]):
     import requests
     import zipfile
 
-    URL = f"https://rhods-public.s3.amazonaws.com/sample-data/accident-data/accident-{dataset_type}.zip"
+    rhods_public = "https://rhods-public.s3.amazonaws.com"
+    dataset_path = f"sample-data/accident-data/accident-{dataset_type}.zip"
+    URL = f"https://{rhods_public}/{dataset_path}"
 
     print("Downloading file...")
     response = requests.get(URL, stream=True)
     block_size = 1024
-    with open(f'{datasets_zip.path}/accident-{dataset_type}.zip', 'wb') as f:
+    with open(f'./accident-{dataset_type}.zip', 'wb') as f:
         for data in response.iter_content(block_size):
             f.write(data)
-        
-    if os.path.exists(f'{datasets_zip.path}/accident-{dataset_type}.zip'):
-        print("Unzipping file...")
-        with zipfile.ZipFile(f'{datasets_zip.path}/accident-{dataset_type}.zip', 'r') as zip_ref:
-            zip_ref.extractall(path=datasets_dir.path)
+
+    print("Unzipping file...")
+    with zipfile.ZipFile(f'./accident-{dataset_type}.zip', 'r') as zip_ref:
+        zip_ref.extractall(path=datasets.path)
     print("Done!")
 
 
 @dsl.component(
     base_image=base_image,
-    packages_to_install=["pandas", "scikit-learn"],
+    packages_to_install=["ultralytics", "opencv-python-headless"],
 )
-def data_prep(
-    x_train_file: dsl.Output[dsl.Dataset],
-    x_test_file: dsl.Output[dsl.Dataset],
-    y_train_file: dsl.Output[dsl.Dataset],
-    y_test_file: dsl.Output[dsl.Dataset],
-):
-    import pickle
+def train_model(datasets: dsl.Input[dsl.Dataset],
+                model_onnx: dsl.Output[dsl.Model]):
+    import os
+    import shutil
+    import datetime
+    from ultralytics import YOLO
 
-    import pandas as pd
-    from sklearn import datasets
-    from sklearn.model_selection import train_test_split
+    print("setting the symlink for the datasets")
+    os.symlink(datasets.path, "/opt/app-root/src/datasets")
 
-    def get_iris_data() -> pd.DataFrame:
-        iris = datasets.load_iris()
-        data = pd.DataFrame(
-            {
-                "sepalLength": iris.data[:, 0],
-                "sepalWidth": iris.data[:, 1],
-                "petalLength": iris.data[:, 2],
-                "petalWidth": iris.data[:, 3],
-                "species": iris.target,
-            }
-        )
+    # load a pretrained model (recommended for training)
+    print("using a base model to start the training")
+    model = YOLO('yolov8m.pt')
+    print("training the model")
+    model.train(data=f'{datasets.path}/accident-sample/data.yaml',
+                epochs=1, imgsz=640, batch=2)
 
-        print("Initial Dataset:")
-        print(data.head())
+    print("saving the file as onnx")
 
-        return data
+    # create runs/detect/train/weights/best.onnx
+    YOLO("/opt/app-root/src/runs/detect/train/weights/best.pt").export(
+        format="onnx")
 
-    def create_training_set(dataset: pd.DataFrame, test_size: float = 0.3):
-        # Features
-        x = dataset[["sepalLength", "sepalWidth", "petalLength", "petalWidth"]]
-        # Labels
-        y = dataset["species"]
-
-        # Split dataset into training set and test set
-        x_train, x_test, y_train, y_test = train_test_split(
-            x, y, test_size=test_size, random_state=11)
-
-        return x_train, x_test, y_train, y_test
-
-    def save_pickle(object_file, target_object):
-        with open(object_file, "wb") as f:
-            pickle.dump(target_object, f)
-
-    dataset = get_iris_data()
-    x_train, x_test, y_train, y_test = create_training_set(dataset)
-
-    save_pickle(x_train_file.path, x_train)
-    save_pickle(x_test_file.path, x_test)
-    save_pickle(y_train_file.path, y_train)
-    save_pickle(y_test_file.path, y_test)
+    # save best.onnx as accident-detection_{timestamp}.onnx
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
+    os.makedirs(model_onnx.path, exist_ok=True)
+    shutil.copy('/opt/app-root/src/runs/detect/train/weights/best.onnx',
+                f'{model_onnx.path}/accident-detection_{timestamp}.onnx')
 
 
 @dsl.component(
     base_image=base_image,
-    packages_to_install=["pandas", "scikit-learn"],
+    packages_to_install=["boto3"],
 )
-def validate_data():
-    pass
+def upload_to_s3(model_onnx: dsl.Input[dsl.Model]):
+    import os
+    import boto3
+    from botocore.client import Config
 
+    print("configuring s3 instance")
+    # Configuration
+    minio_url = "http://minio.object-datastore.svc.cluster.local:9000"
+    access_key = "minio"
+    secret_key = "minio123"
 
-@dsl.component(
-    base_image=base_image,
-    packages_to_install=["pandas", "scikit-learn"],
-)
-def train_model(
-    x_train_file: dsl.Input[dsl.Dataset],
-    y_train_file: dsl.Input[dsl.Dataset],
-    model_file: dsl.Output[dsl.Model],
-):
-    import pickle
+    # Setting up the MinIO client
+    s3 = boto3.client(
+        's3',
+        endpoint_url=minio_url,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        config=Config(signature_version='s3v4'),
+    )
 
-    import pandas as pd
-    from sklearn.ensemble import RandomForestClassifier
-
-    def load_pickle(object_file):
-        with open(object_file, "rb") as f:
-            target_object = pickle.load(f)  # noqa: S301
-
-        return target_object
-
-    def save_pickle(object_file, target_object):
-        with open(object_file, "wb") as f:
-            pickle.dump(target_object, f)
-
-    def train_iris(x_train: pd.DataFrame, y_train: pd.DataFrame):
-        model = RandomForestClassifier(n_estimators=100)
-        model.fit(x_train, y_train)
-
-        return model
-
-    x_train = load_pickle(x_train_file.path)
-    y_train = load_pickle(y_train_file.path)
-
-    model = train_iris(x_train, y_train)
-
-    save_pickle(model_file.path, model)
-
-
-@dsl.component(
-    base_image=base_image,
-    packages_to_install=["pandas", "scikit-learn"],
-)
-def evaluate_model(
-    x_test_file: dsl.Input[dsl.Dataset],
-    y_test_file: dsl.Input[dsl.Dataset],
-    model_file: dsl.Input[dsl.Model],
-    mlpipeline_metrics_file: dsl.Output[dsl.Metrics],
-):
-    import json
-    import pickle
-
-    from sklearn.metrics import accuracy_score
-
-    def load_pickle(object_file):
-        with open(object_file, "rb") as f:
-            target_object = pickle.load(f)  # noqa: S301
-
-        return target_object
-
-    x_test = load_pickle(x_test_file.path)
-    y_test = load_pickle(y_test_file.path)
-    model = load_pickle(model_file.path)
-
-    y_pred = model.predict(x_test)
-
-    accuracy_score_metric = accuracy_score(y_test, y_pred)
-    print(f"Accuracy: {accuracy_score_metric}")
-
-    metrics = {
-        "metrics": [
-            {
-                "name": "accuracy-score",
-                "numberValue": accuracy_score_metric,
-                "format": "PERCENTAGE",
-            },
-        ]
-    }
-
-    with open(mlpipeline_metrics_file.path, "w") as f:
-        json.dump(metrics, f)
-
-
-@dsl.component(
-    base_image=base_image,
-    packages_to_install=["pandas", "skl2onnx"],
-)
-def model_to_onnx(model_file: dsl.Input[dsl.Model], 
-                  onnx_model_file: dsl.Output[dsl.Model]):
-    import pickle
-
-    from skl2onnx import to_onnx
-    from skl2onnx.common.data_types import FloatTensorType
-
-    def load_pickle(object_file):
-        with open(object_file, "rb") as f:
-            target_object = pickle.load(f)  # noqa: S301
-
-        return target_object
-
-    model = load_pickle(model_file.path)
-
-    initial_type = [("float_input", FloatTensorType([None, 4]))]
-    onnx_model = to_onnx(model, initial_types=initial_type)
-
-    with open(onnx_model_file.path, "wb") as f:
-        f.write(onnx_model.SerializeToString())
-
-
-@dsl.component(
-    base_image=base_image,
-    packages_to_install=["pandas", "onnxruntime"],
-)
-def validate_model(onnx_model_file: dsl.Input[dsl.Model]):
-    import onnxruntime
-
-    session = onnxruntime.InferenceSession(onnx_model_file.path)
-
-    input_name = session.get_inputs()[0].name
-    label_name = session.get_outputs()[0].name
-
-    input_values = [[5, 3, 1.6, 0.2]]
-
-    print(f"Performing test prediction on {input_values}")
-    result = session.run([label_name], {input_name: input_values})[0]
-
-    print(f"Response: {result}")
+    for (dirpath, dirnames, filenames) in os.walk(model_onnx.path):
+        for file in filenames:
+            print(f"uploading file {dirpath}/{file}")
+            s3.upload_file(f"{dirpath}/{file}", "models",
+                           f"accident_model/{file}")
 
 
 @kfp.dsl.pipeline(
-    name="Iris Pipeline",
+    name="Accident Detection",
 )
-def iris_pipeline(model_obc: str = "iris-model"):
-    
-    download_data(dataset_type="sample")
-
-    data_prep_task = data_prep()
-
-    train_model_task = train_model(
-        x_train_file=data_prep_task.outputs["x_train_file"],
-        y_train_file=data_prep_task.outputs["y_train_file"],
-    )
-
-    evaluate_model_task = evaluate_model(  # noqa: F841
-        x_test_file=data_prep_task.outputs["x_test_file"],
-        y_test_file=data_prep_task.outputs["y_test_file"],
-        model_file=train_model_task.output,
-    )
-
-    model_to_onnx_task = model_to_onnx(  # noqa: F841
-        model_file=train_model_task.output,
-    )
-
-    validate_model(onnx_model_file=model_to_onnx_task.output)  # noqa: F841
+def accident_detection_pipeline(model_obc: str = "accident-detection"):
+    download_data_task = download_data(dataset_type="sample")
+    train_model_task = train_model(datasets=download_data_task.output)
+    upload_to_s3(model_onnx=train_model_task.outputs["model_onnx"])
 
 
 if __name__ == "__main__":
@@ -284,7 +137,8 @@ if __name__ == "__main__":
         ssl_ca_cert=ssl_ca_cert,
     )
     result = client.create_run_from_pipeline_func(
-        iris_pipeline, arguments={}, experiment_name="iris")
+        accident_detection_pipeline, arguments={},
+        experiment_name="accident detection")
     # Wait 20 minutes for the pipeline to complete
     client.wait_for_run_completion(run_id=result.run_id, timeout=1200)
     print(f"Starting pipeline run with run_id: {result.run_id}")
